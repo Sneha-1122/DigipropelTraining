9bd241a18c70f61c63107b2c849156fb2024a07e
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-06-25 11:55:24 +0530

    Add files via upload

---
+++ 25-06.cs
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+    string name;
+    Console.WriteLine("Enter your name: ");
+    name = Console.ReadLine();
+    Console.WriteLine("Your name is: "+name);
+  }
+}
