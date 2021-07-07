695b0a8b7bc6ad360fed199fd96ff9be06bd9d37
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-07 16:41:28 +0530

    Update June28_3.cs

--- June28_3.cs
+++ June28_3.cs
@@ -9,10 +9,14 @@ Code, Compile, Run and Debug online from anywhere in world.
 using System;
 class HelloWorld {
   static void Main() {
+	  
+	// Program to find the frequency of each element in the given array
+	// Creating arrays of type int and size 100
+	  
  	int[] arr1 = new int[100];
 	int[] freq = new int[100];
     int n, i, j, count;
-    
+	  
    Console.Write("Input the number of elements to be stored in the array :");
    n = Convert.ToInt32(Console.ReadLine()); 	   
 
@@ -22,6 +26,8 @@ class HelloWorld {
       arr1[i] = Convert.ToInt32(Console.ReadLine()); 		  
 	  freq[i] = -1;
     }
+	  
+    // Checking the occurence of every element in the array, and incrementing the count
     for(i=0; i<n; i++)
     {
         count = 1;
@@ -47,3 +53,20 @@ class HelloWorld {
     }
 }
 }
+
+/* 
+INPUT:
+Input the number of elements to be stored in the array :6
+Input 6 elements in the array : 2
+3
+4
+1
+2
+3
+
+OUTPUT:
+2 occurs 2 times
+3 occurs 2 times
+4 occurs 1 times
+1 occurs 1 times
+*/
