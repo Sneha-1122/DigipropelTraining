f617a525c6f3a697baac7f9e550aba35c5d11c02
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-07 16:20:52 +0530

    Update June28_1.cs

--- June28_1.cs
+++ June28_1.cs
@@ -10,18 +10,46 @@ using System;
 class HelloWorld {
   static void Main() {
     Console.WriteLine("Hello World");
+    
+    //Program to add elements of the given array
+    
     Console.WriteLine("Input the number of elements to be stored in the array: ");
     int arraySize = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Input the elements: ");
     int[] arr = new int[10];
     int sum = 0;
-    for(int i=0; i<arraySize; i++) {
-        arr[i] = Convert.ToInt32(Console.ReadLine());
-    }
+    
+    // Storing elements into the array
     
     for(int i=0; i<arraySize; i++) {
-        sum+=arr[i];
+        
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        
     }
+    
+    // Adding up elements in the given array
+    
+    for(int i=0; i<arraySize; i++) {
+        
+        sum+=arr[i];
+        
+    }
+    
   Console.WriteLine("The sum of all elements is: "+sum);
+  
   }
 }
+
+/* 
+INPUT:
+Input the number of elements to be stored in the array:
+4
+Input the elements:
+3
+6
+2
+4
+
+OUTPUT:
+The sum of all elements is: 15 
+*/
