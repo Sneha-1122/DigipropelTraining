4ecff9cd8a7128ad3986b7527ae3512b247a99e3
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-08 11:09:47 +0530

    Update June25_2.cs

--- June25_2.cs
+++ June25_2.cs
@@ -9,14 +9,15 @@ Code, Compile, Run and Debug online from anywhere in world.
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    
+    // Program to add, subtract, multiply and divide two numbers
     int num1, num2, num3,result;
     Console.Write("Enter your first number: ");
     num1 = Convert.ToInt32(Console.ReadLine());
     Console.Write("Enter your second number: ");
     num2 = Convert.ToInt32(Console.ReadLine());
-    //Console.Write("Enter your third number: ");
-    //num3 = Convert.ToInt32(Console.ReadLine());
+    
+    // Performing the operations
     result = num1+num2;
     Console.WriteLine("Adding 2 numbers: "+ result);
     result = num1-num2;
@@ -27,3 +28,15 @@ class HelloWorld {
     Console.WriteLine("Dividing 2 numbers: "+ result);
   }
 }
+
+/*
+INPUT: 
+Enter your first number: 6
+Enter your second number: 5
+
+OUTPUT:
+Adding 2 numbers: 11
+Subtracting 2 numbers: 1
+Multiplying 2 numbers: 30
+Dividing 2 numbers: 1
+*/
