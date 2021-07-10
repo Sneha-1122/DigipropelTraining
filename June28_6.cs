3cca95134fabeb72371aecb195ad38555eeb3aae
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-10 12:25:51 +0530

    Update June28_6.cs

--- June28_6.cs
+++ June28_6.cs
@@ -11,18 +11,31 @@ using System.Linq;
 class HelloWorld {
   static void Main(string[] args)
           {
+
+            // Program to find the missing number in a given array of numbers between 10 and 20.
+
+            Console.WriteLine("Program to find the missing number in a given array of numbers between 10 and 20.");
+            
+            // Creating and initialising the array
             int[] nums = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            // Printing the initialised array
             Console.WriteLine("Original array elements:");
             Array.ForEach(nums, Console.WriteLine);
+
+            //Calling the respective method
             Console.WriteLine("Missing number in the said array (10-20): "+ missingEle(nums));
+
             int[] nums1 = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
             Console.WriteLine("Original array elements:");
             Array.ForEach(nums1, Console.WriteLine);
             Console.WriteLine("Missing number in the said array (10-20): " + missingEle(nums1));
+
             int[] nums2 = { 10, 11, 12, 13, 14, 16, 17, 18, 19, 20 };
             Console.WriteLine("Original array elements:");
             Array.ForEach(nums2, Console.WriteLine);
             Console.WriteLine("Missing number in the said array (10-20): " + missingEle(nums2));
+
             int[] nums3 = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
             Console.WriteLine("Original array elements:");
             Array.ForEach(nums3, Console.WriteLine);
@@ -30,8 +43,63 @@ class HelloWorld {
 
         }
 
+        // Method to find out the missing element in the array
         public static int missingEle(int[] arr)
         {
             return 165 - arr.Sum();
         }
 }
+
+/*
+INPUT:
+Program to find the missing number in a given array of numbers between 10 and 20.
+Original array elements:
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+Missing number in the said array (10-20): 0
+Original array elements:
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+Missing number in the said array (10-20): 10
+Original array elements:
+10
+11
+12
+13
+14
+16
+17
+18
+19
+20
+Missing number in the said array (10-20): 15
+Original array elements:
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+Missing number in the said array (10-20): 20
+*/
