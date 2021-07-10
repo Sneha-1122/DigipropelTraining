532e972d4ef5e8fa5432abe175febd660063b4f5
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-10 11:31:30 +0530

    Update June25_5.cs

--- June25_5.cs
+++ June25_5.cs
@@ -9,26 +9,47 @@ Code, Compile, Run and Debug online from anywhere in world.
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
-    Console.WriteLine("Please enter your input: ");
+    // Program to compute the sum of two given integers, if two values are equal then return the triple of their sum
+    Console.WriteLine("Program to compute the sum of two given integers, if two values are equal then return the triple of their sum");
+    Console.WriteLine("\nPlease enter your input: ");
     int result;
-    string[] tokens = Console.ReadLine().Split();
+    Console.Write("\nEnter the First number: ");
 
-    //Parse element 0
-    
-    int a = int.Parse(tokens[0]);
-    
-    //Parse element 1
-    
-    int b = int.Parse(tokens[1]);
+    // Reading inputs from console
+    int a = Convert.ToInt32(Console.ReadLine());
+ 
+     Console.Write("\nEnter the Second number: ");
+    int b = Convert.ToInt32(Console.ReadLine());
     
     if (a==b) {
         result = (a+b)*3;
-        Console.WriteLine("Your result is: "+result);
+        Console.WriteLine("Triple the sum of two equal integers: "+result);
     }
     else {
         result = a+b;
-        Console.WriteLine("Sum of two unequal numbers: "+result);
+        Console.WriteLine("Sum of two unequal integers: "+result);
     }
   }
 }
+
+/*
+INPUT 1:
+Please enter your input:
+
+Enter the First number: 5
+
+Enter the Second number: 4
+
+OUTPUT 1:
+Sum of two unequal numbers: 9
+
+INTPUT 2:
+Please enter your input:
+
+Enter the First number: 5
+
+Enter the Second number: 5
+
+OUTPUT 2:
+Triple the sum of two equal integers: 30
+*/
