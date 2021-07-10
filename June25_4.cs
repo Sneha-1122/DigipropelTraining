69e0100334c1106668319628dae392edc5594bfb
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-10 11:23:20 +0530

    Update June25_4.cs

--- June25_4.cs
+++ June25_4.cs
@@ -22,3 +22,25 @@ class HelloWorld {
     Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
   }
 }
+
+/*
+INPUT 1:
+Input first integer:
+-4
+Input second integer:
+0
+
+OUTPUT 1:
+Check if one is negative and one is positive:
+False
+
+INPUT 2:
+Input first integer:
+3
+Input second integer:
+-2
+
+OUTPUT 2:
+Check if one is negative and one is positive:
+True
+*/
