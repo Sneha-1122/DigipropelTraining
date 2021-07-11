b0cc5572bdd1f8ae42f370b30e7e9f7df6de4d43
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-11 11:01:27 +0530

    Update June28_8.cs

--- June28_8.cs
+++ June28_8.cs
@@ -9,20 +9,59 @@ Code, Compile, Run and Debug online from anywhere in world.
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
-    Console.WriteLine("Input the number of elements to be stored in the array: ");
+    
+    // Program to read n number of values in an array and display it in reverse order.
+    Console.WriteLine("Program to read n number of values in an array and display it in reverse order.");
+    Console.WriteLine("\nInput the number of elements to be stored in the array: ");
     int arraySize = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Input the elements: ");
     int[] arr = new int[10];
+
+    // Storing elements into the array
     for(int i=0; i<arraySize; i++) {
         arr[i] = Convert.ToInt32(Console.ReadLine());
     }
+
+    // Printing the stored elements
+    Console.WriteLine(" Original array: ");
     for(int i=0; i<arraySize; i++) {
-        Console.WriteLine(" Element {0} ",arr[i]);
+
+        Console.WriteLine(" {0} ",arr[i]);
+
     }
+
+    // Reversing the order of the elements
+    Console.WriteLine("Reversed array: ");
     for(int i = arraySize-1; i>=0; i--){
-        Console.WriteLine("Element {0} ",arr[i]);
+
+        Console.WriteLine(" {0} ",arr[i]);
+      
     }
-  //Console.WriteLine("The sum of all elements is: "+sum);
   }
 }
+
+/*
+INPUT:
+Input the number of elements to be stored in the array:
+5
+Input the elements: 
+3
+5
+7
+3
+2 
+
+OUTPUT:
+Original array: 
+ 3
+ 5
+ 7
+ 3
+ 2
+Reversed array:
+ 2
+ 3
+ 7
+ 5
+ 3
+ */
