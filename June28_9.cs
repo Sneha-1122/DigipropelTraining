d979025b96c11cd504728ec67bbafb60d3b7d85f
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-11 11:09:47 +0530

    Update June28_9.cs

--- June28_9.cs
+++ June28_9.cs
@@ -9,15 +9,23 @@ Code, Compile, Run and Debug online from anywhere in world.
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    
+    // Program to sort elements of array in ascending order
+    Console.WriteLine("\nProgram to sort elements of array in ascending order");
     Console.WriteLine("Input the number of elements to be stored in the array: ");
     int arraySize = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Input the elements: ");
     int[] arr = new int[10];
     int j, temp;
+
+    // Storing the elements in the array
     for(int i=0; i<arraySize; i++) {
+
         arr[i] = Convert.ToInt32(Console.ReadLine());
+
     }
+
+    // Sorting the elements in ascending order
     for(int i=0; i<arraySize; i++)
     {
         for(j=i+1; j<arraySize; j++)
@@ -30,14 +38,33 @@ class HelloWorld {
             }
         }
     }
+
+    // Printing the sorted elements
     Console.WriteLine("Elements of array in ascending order: ");
     for(int i=0;i<arraySize;i++) {
+
         Console.WriteLine(arr[i]);
+
     }
-    
-/*    for(int i = arraySize-1; i>=0; i--){
-        Console.WriteLine("Element {0} ",arr[i]);
-    }*/
-  //Console.WriteLine("The sum of all elements is: "+sum);
   }
 }
+
+/*
+INPUT
+Input the number of elements to be stored in the array: 
+5
+Input the elements: 
+3
+4
+7
+2
+9
+
+OUTPUT:
+Elements of array in ascending order: 
+2
+3
+4
+7
+9
+*/
