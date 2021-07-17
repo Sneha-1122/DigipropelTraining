47d4707533e67fc32effdbb9fcc2e938beb3738e
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-17 16:34:57 +0530

    Update June30_7.cs

--- June30_7.cs
+++ June30_7.cs
@@ -1,28 +1,36 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 using System;
 using System.Linq;
-class HelloWorld
+class StringCapitalize
 {
-    //enum Level{High,Medium,Low}
     static void Main()
     {
+        // Program to convert the first character of each word of a given string to uppercase.
+        Console.WriteLine("Program to convert the first character of each word of a given string to uppercase");
         string str = "python exercises";
-        Console.WriteLine("Original string: " + str);
+        Console.WriteLine("\nOriginal string: " + str);
         Console.WriteLine("After converting the first character of each word of the said string:\n" + Caps(str));
         str = "The quick brown Fox jumps over the little lazy Dog";
         Console.WriteLine("\nOriginal string: " + str);
         Console.WriteLine("After converting the first character of each word of the said string:\n" + Caps(str));
     }
+
+    // Method to split the whole string, Capitalize the first character of every word and then join the words back to a sentence
     public static string Caps(string str)
     {
         return string.Join(" ", str.Split(' ').Select(str => char.ToUpper(str[0]) + str.Substring(1)));
     }
 
 }
+
+/*
+OUTPUT:
+Program to convert the first character of each word of a given string to uppercase
+
+Original string: python exercises
+After converting the first character of each word of the said string:
+Python Exercises
+
+Original string: The quick brown Fox jumps over the little lazy Dog
+After converting the first character of each word of the said string:
+The Quick Brown Fox Jumps Over The Little Lazy Dog
+*/
