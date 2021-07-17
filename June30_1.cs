ccfa5dbfd49b4a7c128057ca20f64eb3f0d18281
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-17 16:14:58 +0530

    Update June30_1.cs

--- June30_1.cs
+++ June30_1.cs
@@ -1,16 +1,11 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 using System;
-class HelloWorld {
+class EnumProgram {
+
+    // Creating an enum
     enum Level{High,Medium,Low}
     static void Main() {
-    Console.WriteLine("Hello World");
+
+    // Using enum
     Level complexity = Level.Medium;
     switch(complexity) 
     {
@@ -26,3 +21,8 @@ class HelloWorld {
   }
   }
 }
+
+/*
+OUTPUT:
+Medium level
+*/
