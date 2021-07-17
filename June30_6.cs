1a5f2d8aedc210b3d764003ae0bc04b722c1b518
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-17 16:28:58 +0530

    Update June30_6.cs

--- June30_6.cs
+++ June30_6.cs
@@ -7,21 +7,36 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 using System;
-class HelloWorld
+class StringPosition
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
-        Console.WriteLine("Program to find the position of a specific word in a string");
-        Console.WriteLine("Please enter your string input: ");
+        // Program to find the position of a specific word in a string
+        Console.WriteLine("\nProgram to find the position of a specific word in a string");
+        Console.WriteLine("\nPlease enter your string input: ");
         string str = "The quick brown fox jumps over the lazy dog";
-      Console.WriteLine("Original string: " + str);
+        Console.WriteLine("Original string: " + str);
         Console.WriteLine("Position of the word 'fox' in the said string: " + posCheck(str, "quick"));
         Console.WriteLine("Position of the word 'The' in the said string: " + posCheck(str, "jumps"));
         Console.WriteLine("Position of the word 'lazy' in the said string: " + posCheck(str, "dog"));
     }
+
+    // Method to pass the sentence input and find the position of the required string
     public static int posCheck(string text, string word)
     {
         return Array.IndexOf(text.Split(' '), word) + 1;
     }
 }
+
+/* 
+INPUT:
+Program to find the position of a specific word in a string
+
+Please enter your string input:
+Original string: The quick brown fox jumps over the lazy dog
+
+OUTPUT:
+Position of the word 'fox' in the said string: 2
+Position of the word 'The' in the said string: 5
+Position of the word 'lazy' in the said string: 9
+*/
