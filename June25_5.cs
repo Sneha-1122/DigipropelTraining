20ba66a45209a715db1b1e014f5bad09addceacd
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-18 10:42:26 +0530

    Update June25_5.cs

--- June25_5.cs
+++ June25_5.cs
@@ -1,14 +1,8 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 using System;
-class HelloWorld {
+class IntegerCheck {
+  
   static void Main() {
+    
     // Program to compute the sum of two given integers, if two values are equal then return the triple of their sum
     Console.WriteLine("Program to compute the sum of two given integers, if two values are equal then return the triple of their sum");
     Console.WriteLine("\nPlease enter your input: ");
