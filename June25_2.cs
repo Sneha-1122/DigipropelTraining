c7787265307dd3ddb9afc35fd2660772d0894696
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-18 10:38:40 +0530

    Update June25_2.cs

--- June25_2.cs
+++ June25_2.cs
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 using System;
-class HelloWorld {
+class Operations {
   static void Main() {
     
     // Program to add, subtract, multiply and divide two numbers
