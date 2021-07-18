c5efaecd0ae82ed7af0c4cdf8daf3494a35c41e1
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-18 10:35:49 +0530

    Update June30_9.cs

--- June30_9.cs
+++ June30_9.cs
@@ -1,26 +1,30 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 using System;
 using System.Linq;
-class HelloWorld
+class StringReverse
 {
-    //enum Level{High,Medium,Low}
     static void Main(string[] args)
     {
+        // Program to reverse all the words of a given string which have even length
         Console.WriteLine("Program to reverse the string words with even length");
         string str = "C# is used to develop web apps , desktop apps , mobile apps , games and much more.";
-        Console.WriteLine("Original string: " + str);
-        Console.WriteLine("Reversed string: " + reverseMethod(str));
+        Console.WriteLine("\nOriginal string: " + str);
+        Console.WriteLine("\nReversed string: " + reverseMethod(str));
     }
+
+    // Method to reverse a string based on the length of it
     public static string reverseMethod(string text)
     {
+        // Splitting the sentence, reversing the respective strings and joining them back together
         return string.Join(" ", text.Split(' ').Select(str => str.Length % 2 != 0 ? str : new String(str.Reverse().ToArray())));
     }
 
 }
+
+/*
+OUTPUT:
+Program to reverse the string words with even length
+
+Original string: C# is used to develop web apps , desktop apps , mobile apps , games and much more.
+
+Reversed string: #C si desu ot develop web sppa , desktop sppa , elibom sppa , games and hcum more.
+*/
