0569fa672fc49344350932c541ceb9944c6661ec
Sneha-1122 <86397820+Sneha-1122@users.noreply.github.com>
2021-07-18 10:38:01 +0530

    Update June25_1.cs

--- June25_1.cs
+++ June25_1.cs
@@ -1,14 +1,7 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 using System;
-class HelloWorld {
+class Multiply {
   static void Main() {
+    
     // Program to multiply three numbers
     Console.WriteLine("Hello World");
    
